af46d4a1d9726900a7d3fd1d5815934bda90c7a1
Yagato <Yagatomon@gmail.com>
2022-05-07 21:35:43 -0500

    Added more replies

--- main.py
+++ main.py
@@ -19,5 +19,20 @@ async def on_message(message):
     if message.content.startswith("$ola"):
         await message.channel.send("Olaaa :wave:")
 
-load_dotenv(os.path.join(os.getcwd(), '.env'))
+    if message.content.startswith("$ogayu"):
+        with open('res/ola_okayu.png', 'rb') as f:
+            picture = discord.File(f)
+            await message.channel.send(file=picture)
+
+    if message.content.startswith("$sui"):
+        with open('res/ola_suisei.jpg', 'rb') as f:
+            picture = discord.File(f)
+            await message.channels.end(file=picture)
+    
+    if "mucho texto" in message.content:
+        with open('res/mucho_texto.mp4', 'rb') as f:
+            picture = discord.File(f)
+            await message.reply(file=picture)
+
+load_dotenv()
 client.run(os.getenv('TOKEN'))
